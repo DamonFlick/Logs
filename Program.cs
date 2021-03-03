97a40f6ac13b9377683e666048a23c67c9de2f95
DamonFlick <75501983+DamonFlick@users.noreply.github.com>
2021-03-02 22:30:53 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plese enter a number.");
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Elite\Desktop\log.txt", true))
+            {
+                file.WriteLine(Convert.ToInt32(Console.ReadLine()));
+            }
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Elite\Desktop\log.txt"));
+            Console.ReadLine();
+        }
+    }
+}
